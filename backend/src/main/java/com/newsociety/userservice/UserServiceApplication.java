474949e5f5ab4bf8cbb7f9bc28b5d4d8eaff1bef
rishit30 <49200552+rishit30@users.noreply.github.com>
2024-12-14 10:30:44 +0530

    Add main application class and configuration

---
+++ backend/src/main/java/com/newsociety/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.newsociety.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
