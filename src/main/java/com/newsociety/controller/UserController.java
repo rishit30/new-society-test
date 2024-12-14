8770f67608083cbc8c11f786d6adffc1bc2fe2ee
rishit30 <49200552+rishit30@users.noreply.github.com>
2024-12-14 10:36:23 +0530

    Add controller and security configuration

---
+++ src/main/java/com/newsociety/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.newsociety.controller;
+
+import com.newsociety.model.User;
+import com.newsociety.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        try {
+            User registeredUser = userService.registerUser(user);
+            return ResponseEntity.ok(registeredUser);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
