056cbcd57399d4c54892d9d8e858fe84c18684aa
rishit30 <49200552+rishit30@users.noreply.github.com>
2024-12-14 10:36:11 +0530

    Add User service

--- src/main/java/com/newsociety/service/UserService.java
+++ src/main/java/com/newsociety/service/UserService.java
@@ -1,29 +1,27 @@
 package com.newsociety.service;
 
-import com.newsociety.dto.UserRegistrationRequest;
 import com.newsociety.model.User;
 import com.newsociety.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
 
-    public User registerUser(UserRegistrationRequest request) {
-        if (userRepository.existsByEmail(request.getEmail())) {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
             throw new RuntimeException("Email already exists");
         }
 
-        User user = new User();
-        user.setName(request.getName());
-        user.setEmail(request.getEmail());
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-
+        // Encode password before saving
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
